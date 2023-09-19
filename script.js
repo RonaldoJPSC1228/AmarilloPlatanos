d6b1f38b3008c6e6449baa1f65eefa4508c68b28
RonaldoJps1228 <rsuarez13@cuc.edu.co>
2023-09-18 23:57:39 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("mostrarImagen").addEventListener("click", function() {
+    var imagenOculta = document.querySelector(".imagen-oculta");
+    imagenOculta.style.display = "block"; // Mostrar la imagen
+
+    var mensaje = "Aqui tienes tus flores nomas por bonita :B";
+    alert(mensaje);
+});
